fa676d0cfa05212f5e4ccebbe4a6c1658d2cba7b
sahil-kamble <157009104+sahil-kamble@users.noreply.github.com>
2025-03-20 04:36:35 +0000

    update button

--- js/main.js
+++ js/main.js
@@ -130,3 +130,21 @@
 
 })(jQuery);
 
+//  button Section
+var button = document.querySelector('.fabTrigger');
+var wrap = document.querySelector('.fabWidget');
+var overlay = document.querySelector('.fabOverlay');
+
+button.onclick = function() {
+  button.classList.toggle('active');
+  wrap.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+overlay.onclick = function() {
+  button.classList.toggle('active');
+  wrap.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+
